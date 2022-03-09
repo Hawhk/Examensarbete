7d00afa65a54421fe39d577c74cb298a587afe8b
Hampus Hurtig <hampus.hurtig@outlook.com>
2022-03-09 12:31:05 +0100

    application runs yay

--- ktor/build.gradle.kts
+++ ktor/build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
     implementation("io.ktor:ktor-freemarker:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-client-core:1.6.7")
+    implementation("io.ktor:ktor-client-cio:1.6.7")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
