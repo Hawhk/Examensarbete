395205cb4c05a1402aa8006aaee8e3c67d0d006b
Hampus Hurtig <hampus.hurtig@outlook.com>
2022-04-06 15:59:35 +0200

    dynamic page working

--- ktor/src/main/kotlin/com/exwork/controllers/Dynamic.kt
+++ ktor/src/main/kotlin/com/exwork/controllers/Dynamic.kt
@@ -10,29 +10,28 @@ import io.ktor.routing.route
 import io.ktor.routing.Route
 import io.ktor.routing.get as routeGet
 // import io.ktor.http.*
-import org.jetbrains.exposed.sql.EmptySizedIterable
-import com.exwork.models.*
+import com.exwork.models.Article
+import com.exwork.models.Articles
+import com.exwork.models.Section
+import com.exwork.models.Sections
+import org.jetbrains.exposed.sql.Table.Dual.integer
+import java.lang.reflect.TypeVariable
+import kotlin.reflect.full.declaredMemberProperties
 
 fun Route.dynamicPages() {
     route("/dynamic") {
         route("/small") {
             routeGet {
                 val article = transaction {
-                     Article.findById(1)
+                    Article.findById(1)
                 }
-                // print(article?.sections)
-                val sections = transaction {
-                    article?.sections?.iterator()
+                val sections = ArrayList<Section>()
+
+                transaction {
+                    article?.sections?.forEach { sections.add(it) }
                 }
 
-                while(sections!!.hasNext()) {
-                    println(sections.next())
-                }
-                // val total = transaction{
-                //     sections?
-                // }
-                // println(sections)
-                call.respondTemplate("dynamic.ftl", mapOf("article" to article))
+                call.respondTemplate("dynamic.ftl", mapOf("article" to article, "sections" to sections))
             }
         }
         route("/large") {
